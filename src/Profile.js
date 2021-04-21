6798e6eb6ac4b7f3ab08ad87b1271bdfa54271c7
Paula Waite <pawaite@madisoncollege.edu>
2021-04-21 14:56:07 -0500

    profile.js with formatting

---
+++ src/Profile.js
@@ -0,0 +1,23 @@
+/* src/Profile.js */
+import React from 'react';
+import './App.css';
+
+import { 
+    withAuthenticator
+    , AmplifySignOut 
+} from '@aws-amplify/ui-react'
+
+const Profile = () => {
+  return (
+    <div style={containerStyle}>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+const containerStyle = {
+  width: 400,
+  margin: '20px auto'
+}
+
+export default withAuthenticator(Profile);  // wrapping the profile component (HOC - higher order component - withAuthenticator)
