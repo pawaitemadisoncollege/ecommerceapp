9e804e85938e392b25d93a32561056a857ff93b4
Paula Waite <pawaite@madisoncollege.edu>
2021-04-21 15:24:03 -0500

    Admin.js from textbook and then modify

---
+++ src/Admin.js
@@ -0,0 +1,88 @@
+import React
+        , { 
+            useState 
+        } 
+from 'react';
+
+import './App.css';
+import { 
+    Input
+    , Button 
+} from 'antd';
+
+import { API } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react'''
+
+const initialState = {
+  name: ''
+  , price: ''
+}
+
+const Admin = () => {
+    const [itemInfo, updateItemInfo] = useState(initialState);
+
+    const updateForm = (e) => {
+        const formData = {
+            ...itemInfo
+            , [e.target.name]: e.target.value
+        }   
+        updateItemInfo(formData);
+    }
+
+    const addItem = async () => {
+        try {
+            const data = {
+                body: { 
+                    ...itemInfo
+                    , price: parseInt(itemInfo.price)   // changing string to int
+                }
+            };
+            // clears the input controls (form)
+            updateItemInfo(initialState)
+        
+            await API.post(
+                'ecommerceapi' // my api name
+                , '/products'
+                , data
+            );
+        } 
+        
+        catch (err) {
+            console.error('error adding item...', err);
+        }
+    ;}
+
+    return (
+        <div style={containerStyle}>
+            <Input
+                name='name'
+                onChange={updateForm}
+                value={itemInfo.name}
+                placeholder='Item name'
+                style={inputStyle}
+            />
+            <Input
+                name='price'
+                onChange={updateForm}
+                value={itemInfo.price}
+                style={inputStyle}
+                placeholder='item price'
+            />
+            <Button
+                style={buttonStyle}
+                onClick={addItem}
+            >
+                Add Product
+            </Button>
+        </div>
+    )S
+}
+
+const containerStyle = { 
+    width: 400
+    , margin: '20px auto' 
+};
+const inputStyle = { marginTop: 10 };
+const buttonStyle = { marginTop: 10 };
+
+export default withAuthenticator(Admin);
